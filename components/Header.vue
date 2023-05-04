c33f104590228c7bd421c2adc9ca01aebecbacd0
tom.meier <81823167+meiert54@users.noreply.github.com>
2023-05-04 14:59:28 +0200

    navbar animation done

--- components/Header.vue
+++ components/Header.vue
@@ -4,6 +4,30 @@ interface IHeaderEmits {
 }
 
 const emit = defineEmits<IHeaderEmits>()
+const indicatorOpacity = ref(0)
+const indicatorWidth = ref(100)
+const indicatorLeft = ref(0)
+
+const indicatorStyle = computed(() => {
+    return {
+        opacity: indicatorOpacity.value,
+        width: indicatorWidth.value + 'px',
+        left: indicatorLeft.value + 'px'
+    }
+})
+
+const hover = (e: MouseEvent) => {
+    let el: any = e.target
+    let width = el.offsetWidth
+    let left = el.offsetLeft
+
+    indicatorWidth.value = width
+    indicatorLeft.value = left
+
+    setTimeout(() => indicatorOpacity.value = .6, 100)
+}
+
+const leave = () => setTimeout(() => indicatorOpacity.value = 0, 100)
 
 const openMenu = () => emit('openMenu')
 </script>
@@ -11,36 +35,40 @@ const openMenu = () => emit('openMenu')
 <template>
     <header>
         <div class="logo">
-            <img src="/img/logo.png" alt="logo">
+            <svg class="tds-icon tds-icon-logo-wordmark tds-site-logo-icon" viewBox="0 0 342 35" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 .1a9.7 9.7 0 0 0 7 7h11l.5.1v27.6h6.8V7.3L26 7h11a9.8 9.8 0 0 0 7-7H0zm238.6 0h-6.8v34.8H263a9.7 9.7 0 0 0 6-6.8h-30.3V0zm-52.3 6.8c3.6-1 6.6-3.8 7.4-6.9l-38.1.1v20.6h31.1v7.2h-24.4a13.6 13.6 0 0 0-8.7 7h39.9v-21h-31.2v-7h24zm116.2 28h6.7v-14h24.6v14h6.7v-21h-38zM85.3 7h26a9.6 9.6 0 0 0 7.1-7H78.3a9.6 9.6 0 0 0 7 7zm0 13.8h26a9.6 9.6 0 0 0 7.1-7H78.3a9.6 9.6 0 0 0 7 7zm0 14.1h26a9.6 9.6 0 0 0 7.1-7H78.3a9.6 9.6 0 0 0 7 7zM308.5 7h26a9.6 9.6 0 0 0 7-7h-40a9.6 9.6 0 0 0 7 7z" fill="currentColor"></path>
+            </svg>
         </div>
         <ul>
-            <li class="item">Model S</li>
-            <li class="item">Model 3</li>
-            <li class="item">Model X</li>
-            <li class="item">Model Y</li>
-            <li class="item">Powerwall</li>
-            <li class="item">Recharge</li>
+            <li @mouseover="hover" @mouseleave="leave" class="item">Model S</li>
+            <li @mouseover="hover" @mouseleave="leave" class="item">Model 3</li>
+            <li @mouseover="hover" @mouseleave="leave" class="item">Model X</li>
+            <li @mouseover="hover" @mouseleave="leave" class="item">Model Y</li>
+            <li @mouseover="hover" @mouseleave="leave" class="item">Powerwall</li>
+            <li @mouseover="hover" @mouseleave="leave" class="item">Recharge</li>
         </ul>
         <ul>
-            <li class="item">Assistance</li>
-            <li class="item">Shop</li>
-            <li class="item">Compte</li>
-            <li @click="openMenu" class="item">Menu</li>
+            <li @mouseover="hover" @mouseleave="leave" class="item">Assistance</li>
+            <li @mouseover="hover" @mouseleave="leave" class="item">Shop</li>
+            <li @mouseover="hover" @mouseleave="leave" class="item">Compte</li>
+            <li @mouseover="hover" @mouseleave="leave" @click="openMenu" class="item">Menu</li>
         </ul>
+        <span :style="indicatorStyle" class="indicator"></span>
     </header>
 </template>
 
 <style lang="scss">
 header {
     position: fixed;
-    padding: .5rem 1.5rem;
+    padding: .7rem 3rem;
     width: 100%;
     display: flex;
     justify-content: space-between;
+    align-items: center;
 
     .logo {
         cursor: pointer;
-        width: 150px;
+        width: 120px;
 
         img {
             width: 100%;
@@ -52,7 +80,7 @@ header {
         list-style: none;
         display: flex;
         align-items: center;
-        
+
         .item {
             cursor: pointer;
             padding: .5rem 1rem;
@@ -60,11 +88,19 @@ header {
             font-size: .9rem;
             font-weight: bold;
             color: var(--font);
-
-            &:hover {
-                background: var(--hover);
-            }
         }
+    }
+
+    .indicator {
+        position: absolute;
+        z-index: -1;
+        opacity: 0;
+        left: 0;
+        width: 100px;
+        height: 33px;
+        background: var(--hover);
+        border-radius: .2rem;
+        transition: .4s cubic-bezier(0.860, 0.160, 0.105, 0.855);
     }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,48 @@
 <template>
     <div class="index-page">
+        <section id="model-y"></section>
+        <section id="model-3"></section>
+        <section id="model-s"></section>
+        <section id="model-x"></section>
+        <section id="powerwall"></section>
+        <section id="accessoires"></section>
     </div>
 </template>
 
 <style lang="scss">
 .index-page {
     width: 100%;
-    height: 100vh;
-    background: url('/img/Homepage-Model-Y-Global-Desktop.png');
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
+    height: 100%;
+
+    section {
+        height: 100vh;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+    #model-y {
+        background-image: url('/img/model-y.png');
+    }
+
+    #model-3 {
+        background-image: url('/img/model-3.png');
+    }
+    
+    #model-s {
+        background-image: url('/img/model-s.png');
+    }
+    
+    #model-x {
+        background-image: url('/img/model-x.png');
+    }
+    
+    #powerwall {
+        background-image: url('/img/powerwall.png');
+    }
+    
+    #accessoires {
+        background-image: url('/img/accessoires.png');
+    }
 }
 </style>
