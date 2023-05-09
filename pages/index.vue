64ec846b736e0eb3d160df070b0c10be8c004929
tom.meier <81823167+meiert54@users.noreply.github.com>
2023-05-09 11:06:46 +0200

    Rename component ModelSection into Section

--- pages/index.vue
+++ pages/index.vue
@@ -6,12 +6,12 @@ const scrollingMode = computed(() => attrs.active ? { overflowY: 'hidden' } : { 
 
 <template>
     <main class="index-page" :style="scrollingMode">
-        <ModelSection :is-model="true" name="y" img="model-y.png" :ncap="true" />
-        <ModelSection :is-model="true" name="3" img="model-3.png" :ncap="true" />
-        <ModelSection :is-model="true" name="s" img="model-s.png" :ncap="false" />
-        <ModelSection :is-model="true" name="x" img="model-x.png" :ncap="false" />
-        <ModelSection :is-model="false" name="powerwall" img="powerwall.png" :ncap="false" />
-        <ModelSection :is-model="false" name="accessoires" img="accessoires.png" :ncap="false" />
+        <Section :is-model="true" name="y" img="model-y.png" :ncap="true" />
+        <Section :is-model="true" name="3" img="model-3.png" :ncap="true" />
+        <Section :is-model="true" name="s" img="model-s.png" :ncap="false" />
+        <Section :is-model="true" name="x" img="model-x.png" :ncap="false" />
+        <Section :is-model="false" name="powerwall" img="powerwall.png" :ncap="false" />
+        <Section :is-model="false" name="accessoires" img="accessoires.png" :ncap="false" />
     </main>
 </template>
 
